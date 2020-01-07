17ef38ad6ef2e2bf50cd78b2da00228e0c9a8577
Zakaria Bilal <zakaria1.bilal@gmail.com>
2020-01-07 12:22:10 +0100

    updated the index js

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,10 @@ app
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
   .use("/api/users", require("./routes/users/route"))
+  .use("/api/meetings", require("./routes/meeting/route"))
+  .use("/api/events", require("./routes/events/route"))
+  .use("api/matieres", require("./routes/matiere/routes"))
+  .use("/api/timetable", require("./routes/timeTable/route"))
   .get("*", (_, res) =>
     res.status(404).json({ success: false, data: "Endpoint not found" })
   );
--- functions/routes/users/route.js
+++ functions/routes/users/route.js
@@ -1,9 +1,24 @@
 const router = require("express").Router();
 const controller = require("./controller");
 
-router.get("/", controller.getUsers);
-router.get("/:id", controller.getUser);
-router.put("/:id", controller.updateUser);
-router.delete("/:id", controller.deleteUser);
+router.get("/test/", controller.getUsers);
+router.get("/test/:id", controller.getUser);
+router.post("/test/", controller.addUser);
+router.put("/test/:id", controller.updateUser);
+router.delete("/test/:id", controller.deleteUser);
+
+const {
+  checkIfAuthenticated,
+  checkIfHasProf,
+  checkIfHasStudent,
+  checkIfHasChef,
+  checkIfHasDelegue
+} = require("../../middleware/auth");
+
+router.get("/", checkIfHasChef, controller.getUsers);
+router.get("/:id", checkIfHasChef, controller.getUser);
+router.post("/", checkIfHasChef, controller.addUser);
+router.put("/:id", checkIfHasChef, controller.updateUser);
+router.delete("/:id", checkIfHasChef, controller.deleteUser);
 
 module.exports = router;
